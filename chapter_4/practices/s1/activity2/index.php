5786aaeb76d5bf0914dc648d6c01b1ec7626d1c9
Hoàng <hoanghung20030@gmail.com>
2023-12-06 17:04:07 +0700

    add

--- chapter_4/practices/s1/activity2/index.php
+++ chapter_4/practices/s1/activity2/index.php
@@ -4,37 +4,57 @@ require_once 'templates/header.php';
 
 <?php
 $host     = 'localhost'; // Because MySQL is running on the same computer as the web server
-$database = 'PHP_connect'; // Name of the database you use (you need first to CREATE DATABASE in MySQL)
+$database = 'myclasspnv25'; // Name of the database you use (you need first to CREATE DATABASE in MySQL)
 $user     = 'root'; // Default username to connect to MySQL is root
-$password = ''; // Default password to connect to MySQL is empty
+$password = 'mysql'; // Default password to connect to MySQL is empty
 
 // TO DO: CREATE CONNECTION TO DATABASE
 // Read file: https://www.w3schools.com/php/php_mysql_connect.asp
-
-$db = 
+try {
+    $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
 ?>
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['username']) && !empty($_POST['message'])) :
     $username = $_POST['username'];
     $message = $_POST['message'];
-    $db->query( // TO DO: INSERT NEW POST IN DATABASE
+    // cách 1:
+    // $query = $db->prepare("INSERT INTO posts (name, message) VALUES (:username, :message)"); 
+    // $query->bindParam(':username', $username);
+    // $query->bindParam(':message', $message);
+    // $query->execute();// TO DO: INSERT NEW POST IN DATABASE
+
+    //cách 2
+    $db->query("INSERT INTO posts (name, message) VALUES ('$username', '$message')");
+
 endif;
 ?>
 
 <?php
 // TO DO: SELECT ALL POSTS FROM DATABASE
+//cách 1:
+// $query = $db->query("SELECT * FROM posts");
+// $posts = $query->fetchAll(PDO::FETCH_ASSOC);
 
-$posts = 
+//cách 2:
+$posts = $db->query("SELECT * FROM posts;");
+
+
 
 foreach ($posts as $post) :
 ?>
     <div class="card">
         <div class="card-header">
-            <span><?php // TO DO: display the value of username for this post ?></span>
+            <span><?php echo $post['name'];// TO DO: display the value of username for this post ?></span>
         </div>
         <div class="card-body">
-            <p class="card-text"><?php // TO DO: display the message for this post ?></p>
+            <p class="card-text"><?php echo $post['message'];// TO DO: display the message for this post ?></p>
         </div>
     </div>
     <hr>
